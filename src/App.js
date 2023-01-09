22fd3c549478085a2024b09f9ae9382c2e38e33a
Harsh Baliyan <harshbaliyan126@gmail.com>
2023-01-09 13:30:40 +0530

    Add css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import Addtask from './Addtask';
-import Showtask from './Showtask';
-import { useState } from 'react';
+import Addtask from "./Addtask";
+import Showtask from "./Showtask";
+import { useState } from "react";
 
 const App = () => {
   const [addtodo, setAddtodo] = useState(false);
-  return ( 
-    <div>
-      <h1>Todo List</h1> 
-      <Addtask addtodo={addtodo} setAddtodo={setAddtodo}/>
-      <Showtask addtodo={addtodo}/>
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: "center", paddingTop: "50px" }}>Todo List</h1>
+      <Addtask addtodo={addtodo} setAddtodo={setAddtodo} />
+      <Showtask addtodo={addtodo} />
     </div>
-   );
-}
- 
+  );
+};
+
 export default App;
--- src/Tasklist.js
+++ src/Tasklist.js
@@ -1,60 +1,77 @@
-const Tasklist = ({tasks, deletetodo, setDeletetodo, edittodo, setEdittodo}) => {
-
-
-    const handleClick = (id) => {
-        fetch(`http://localhost:8000/todo/${id}`, {
-                 method: `Delete`
-            }).then( () => {
-                 console.log(`Delete: Delething this task.`)
-                 if(deletetodo){
-                     setDeletetodo(false);
-                 }
-                 else{
-                     setDeletetodo(true);
-                 }}).catch( () => console.log('Delete: Error!'))
-    }
-
-    const handleCheck = (id, nextStatus, task) => {
-        fetch(`http://localhost:8000/todo/${id}`, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ task ,status : nextStatus})
-            }).then( () => {
-                console.log('PUT: Edit Successfull ! ')
-                if(edittodo){
-                    setEdittodo(false);
-                }
-                else{
-                    setEdittodo(true);
-                }
+const Tasklist = ({
+  tasks,
+  deletetodo,
+  setDeletetodo,
+  edittodo,
+  setEdittodo,
+}) => {
+  const handleClick = (id) => {
+    fetch(`http://localhost:8000/todo/${id}`, {
+      method: `Delete`,
+    })
+      .then(() => {
+        console.log(`Delete: Delething this task.`);
+        if (deletetodo) {
+          setDeletetodo(false);
+        } else {
+          setDeletetodo(true);
         }
-                ).catch( (e) => console.log(e, 'PUT: Error'))
-    }
+      })
+      .catch(() => console.log("Delete: Error!"));
+  };
 
-    return (
-        <div>
-            {tasks.map(t => 
-                <div key={t.id}>
-                    <div>
-                        <input type="checkbox" 
-                           name="tasks"
-                           value={t.id}
-                           checked={t.status}
-                           onChange={e => {
-                            handleCheck(
-                                t.id,
-                                e.target.checked,
-                                t.task
-                            )
-                           }}
-                          />
-                        { t.status === true ? <span><del> {t.task} </del></span> : <span> {t.task} </span> }
-                        <button onClick={ () => handleClick(t.id)}>X</button>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
-}
+  const handleCheck = (id, nextStatus, task) => {
+    fetch(`http://localhost:8000/todo/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ task, status: nextStatus }),
+    })
+      .then(() => {
+        console.log("PUT: Edit Successfull ! ");
+        if (edittodo) {
+          setEdittodo(false);
+        } else {
+          setEdittodo(true);
+        }
+      })
+      .catch((e) => console.log(e, "PUT: Error"));
+  };
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          {tasks.map((t) => (
+            <tr key={t.id}>
+              <th scope="row">
+                <input
+                  type="checkbox"
+                  name="tasks"
+                  value={t.id}
+                  checked={t.status}
+                  onChange={(e) => {
+                    handleCheck(t.id, e.target.checked, t.task);
+                  }}
+                />
+              </th>
+              <td>
+                {t.status === true ? (
+                  <span>
+                    <del> {t.task} </del>
+                  </span>
+                ) : (
+                  <span> {t.task} </span>
+                )}
+              </td>
+              <td>
+                <button onClick={() => handleClick(t.id)}>X</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Tasklist;
